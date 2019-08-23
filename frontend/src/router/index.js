896584f82f1481d04a88be60beab55796ae765eb
Reiji <reiji.asano@konicaminolta.com>
2019-08-23 13:20:13 +0900

    HOME, ABOUT追加

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,15 +1,21 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+
+const routeOptions = [
+  { path: '/', component: 'Home' },
+  { path: '*', component: 'About'}
+]
+
+const routes = routeOptions.map(route => {
+  return {
+    ...route,
+    component: () => import(`@/components/${route.component}.vue`)
+  }
+})
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
+  routes,
+  mode: 'history'
 })
